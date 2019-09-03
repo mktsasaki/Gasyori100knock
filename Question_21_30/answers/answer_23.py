1a3d82d39b956f4fecdb691e7ebecaa28c8ac7ce
NZK181206D <nzk181206d@NZK181206DnoMacBook-Pro.local>
2019-09-03 10:14:20 +0900

    9/11実施予定分までバグ修正&コメント追加

--- Question_21_30/answers/answer_23.py
+++ Question_21_30/answers/answer_23.py
@@ -5,12 +5,16 @@ import matplotlib.pyplot as plt
 # histogram equalization
 def hist_equal(img, z_max=255):
 	H, W, C = img.shape
-	S = H * W * C * 1.
+	S = H * W * C * 1. # 実数化のために1.0を乗算
 
 	out = img.copy()
 
 	sum_h = 0.
 
+	# (R,G,B)を一緒くたに平坦化してしまっているので
+	# 色味とかが結構変わってしまっている
+	# 通常はグレイスケールなどの画像に対して使用するか、RGBのプレーン別に平坦化します。
+	# 全体的にコントラストが強調されるような効果が得られる点は共通している
 	for i in range(1, 255):
 		ind = np.where(img == i)
 		sum_h += len(img[ind])
--- Question_21_30/answers/answer_25.py
+++ Question_21_30/answers/answer_25.py
@@ -10,8 +10,15 @@ def nn_interpolate(img, ax=1, ay=1):
 	aH = int(ay * H)
 	aW = int(ax * W)
 
-	y = np.arange(aH).repeat(aW).reshape(aW, -1)
-	x = np.tile(np.arange(aW), (aH, 1))
+	# numpy.arange(N) : 0から1ずつインクリメントされた長さNの1次元配列を作成 
+	# numpy.repeat(M) : 各要素をM回ずつ繰り返した要素を作成。
+	# numpy.reshape() : データの次元を切り直す(データの並びは変わらない。自明な部分は-1で指定可能。
+	# numpy.tile(a, (h, w)) : データaを(h, w)個並べる(a が1次元の長さlの配列の場合は aは(1, l)の配列として扱う)
+
+	# 出力サイズでの各画素の座標リストを作成
+	y = np.arange(aH).repeat(aW).reshape(-1, aW) # columun数がawになるようにraw数は決定される。→(aH, aW)
+	x = np.tile(np.arange(aW), (aH, 1)) # (1, aW)のデータを(aH, 1)個並べる→(aH, aW)個になる
+	# 出力画像上の座標が入力画像上のどの位置を参照するかをスケーリングして計算
 	y = np.round(y / ay).astype(np.int)
 	x = np.round(x / ax).astype(np.int)
 
